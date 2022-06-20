63c0eb1f7aeaf5f9586ca42d2d3a343d6d807864
taitan02 <102464504+taitan02@users.noreply.github.com>
2022-06-20 12:22:53 +0700

    header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,25 +3,28 @@ import "./App.css";
 import "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import { Home, About, Commit, Tracking, Service, StandardService, FastService } from "./pages/pageExport"
+import { Home, About, Commit } from "./pages/pageExport"
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 const App = () => {
   return (
+
     <Router>
       <div className="wrapper">
         <Header />
-        <Route path="/" exact component={Home} />
-        <Route path="/about" component={About} />
-        <Route path="/commit" component={Commit} />
-        <Route path="/tracking" component={Tracking} />
-        <Route path="/service" component={Service} />
-        <Route path="/standard-service" component={StandardService} />
-        <Route path="/fast-service" component={FastService} />
-
+        <Switch>
+          <Route path="/" exact component={Home} />
+          {/* <Route path="/about" component={About} />
+          <Route path="/commit" component={Commit} />
+          <Route path="/tracking" component={Tracking} />
+          <Route path="/service" component={Service} />
+          <Route path="/standard-service" component={StandardService} />
+          <Route path="/fast-service" component={FastService} /> */}
+        </Switch>
         <Footer />
       </div>
     </Router>
+
   )
 }
 
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -2,39 +2,99 @@ import logoJT from '../assets/icons/logo-J&T.svg'
 import { FaChevronDown, FaBars } from 'react-icons/fa'
 import styled from 'styled-components'
 import { Link } from "react-router-dom"
+import { useState } from 'react'
+import { Menu } from 'antd'
+import 'antd/dist/antd.css'
+
+function getItem(label, key, children) {
+    return {
+        key,
+        children,
+        label,
+    };
+}
+const items = [
+    getItem(<Link to="/">Trang chủ</Link>, 'sub1'),
+    getItem(<Link to="about">Giới thiệu</Link>, 'sub2', [
+        getItem(<Link to="about">Về chúng tôi</Link>, '2'),
+        getItem(<Link to="commit">Cam kết</Link>, '3'),
+    ]),
+    getItem(<Link to="tracking">Tra cứu</Link>, 'sub3', [
+        getItem(<Link to="">Cước vận chuyển</Link>, '4'),
+        getItem(<Link to="">Bưu cục gần đây</Link>, '5'),
+        getItem(<Link to="">Vận đơn</Link>, '6'),
+        getItem(<Link to="">Bảng giá</Link>, '7'),
+        getItem(<Link to="">Hàng cấm gửi</Link>, '8'),
+    ]),
+    getItem(<Link to="service">Dịch vụ</Link>, 'sub4', [
+        getItem(<Link to="" className="flex flex-col leading-5">
+            <span >Dịch vụ chuyển phát tiêu chuẩn</span>
+            <i className=" text-[#F00] font-bold">J&T Epress</i>
+        </Link>, '9'),
+        getItem(<Link to="" className="flex flex-col leading-5">
+            <span >Dịch vụ chuyển phát nhanh</span>
+            <i className=" text-[#F00] font-bold">J&T Fast</i>
+        </Link>, '10'),
+        getItem(<Link to="" className="flex flex-col leading-5">
+            <span >Dịch vụ Siêu giao hàng</span>
+            <i className=" text-[#F00] font-bold">J&T Super</i>
+        </Link>, '11'),
+        getItem(<Link to="" className="flex flex-col leading-5">
+            <span >Dịch vụ Tươi sống</span>
+            <i className=" text-[#F00] font-bold">J&T Fresh</i>
+        </Link>, '12'),
+    ]),
+    getItem(<Link to="">Tuển dụng </Link>, 'sub5', [
+        getItem(<Link to="about">Cơ hội nghề nghiệp</Link>, '13'),
+        getItem(<Link to="commit">Cuộc sống J%T Epress</Link>, '14'),
+    ]),
+    getItem(<Link to="about">Tư vấn</Link>, 'sub6', [
+        getItem(<Link to="about">Liên hệ</Link>, '15'),
+        getItem(<Link to="commit">Đăng kí tư vấn</Link>, '16'),
+    ]),
+];
+const rootSubmenuKeys = ['sub1', 'sub2', 'sub3', 'sub4', 'sub5', 'sub6'];
 
 const Header = () => {
+    const [openKeys, setOpenKeys] = useState(['sub1']);
+    //Logic mobile-navigation --- Còn lỗi
+    const onOpenChange = (keys) => {
+        const latestOpenKey = keys.find((key) => openKeys.indexOf(key) === -1);
+        if (rootSubmenuKeys.indexOf(latestOpenKey) === -1) {
+            setOpenKeys(keys);
+        } else {
+            setOpenKeys(latestOpenKey ? [latestOpenKey] : []);
+        }
+    };
+    // 
+    //Đè lên style ant design
+    const All = styled.div`  
+    .ant-menu{
+    width:75% !important;
+    }`
     const SubMenu = styled.li`
     &:hover ul {
-        display:block
-    }
-    `
-    const displayMenuMobile = () => {
-        const menuMobile = document.querySelector('.mobile-menu')
-        menuMobile.classList.remove('hidden')
-    }
+        display:block !important
+    } `
+    const [isOpen, setIsOpen] = useState(false)
     const removeMobileMenu = (e) => {
-        if (!e.target.closest('.cont')) {
-            const myDropdown = document.querySelector(".mobile-menu");
-            if (!myDropdown.classList.contains('hidden')) {
-                myDropdown.classList.add('hidden');
-            }
+        if (!e.target.closest('.ant-menu') || e.target.closest('a')) {
+            setIsOpen(!isOpen)
         }
     }
+
     return (
-        <div className="fixed bg-white inset-x-0 h-[65px] z-10 ">
+        <All className="fixed bg-white inset-x-0 h-[65px] z-10 shadow-xl">
             <div className=" flex justify-between items-center h-full px-4 lg:px-0 container mx-auto text-sm ">
                 <div className="bt lg:hidden"
-                    onClick={displayMenuMobile}
+                    onClick={() => setIsOpen(!isOpen)}
                 >
                     <FaBars className="w-7 h-7" />
                 </div>
-                <a className="">
+                <Link to="/">
                     <img src={logoJT} className="" />
-                </a>
-                <div className="inline-block invisible lg:hidden">Vi</div>
-
-                <ul className="hidden lg:flex h-full items-center ">
+                </Link>
+                <ul className="hidden lg:flex h-full justify-center items-center m-0">
                     <li >
                         <Link to="/" className=" flex items-center px-4 py-2 rounded-lg hover:bg-gray-200">Trang chủ</Link>
                     </li>
@@ -52,7 +112,7 @@ const Header = () => {
                         </ul>
                     </SubMenu>
                     <SubMenu>
-                        <Link to="tracking" className="inline-flex items-center px-4 py-2">Tra cứu
+                        <Link to="tracking" className="inline-flex items-center  px-4 py-2">Tra cứu
                             <FaChevronDown className="h-4 w-4 pl-[6px]" />
                         </Link>
                         <ul className="hidden absolute bg-white rounded-lg z-10 border shadow-lg">
@@ -114,93 +174,24 @@ const Header = () => {
                         </ul>
                     </SubMenu>
                 </ul>
-            </div >
-            {/* Phần mobile menu */}
-            <div div className="mobile-menu hidden lg:hidden fixed left-0 top-0 right-0 bottom-0 bg-slate-400/40 "
-                onClick={removeMobileMenu}
-            >
-                <div className="cont absolute flex flex-col left-0 top-0 bottom-0 w-3/5 bg-white border text-sm overflow-auto" >
-                    <ul>
-                        <li className="border-b">
-                            <Link to="/" className="inline-flex  items-center px-4 py-2  ">Trang chủ</Link>
-                        </li>
-                        <li>
-                            <div className="flex justify-between items-center border-b">
-                                <Link to="about" className="inline-flex px-4 py-2 ">Giới thiệu</Link>
-                                <FaChevronDown className=" h-3 w-3 mr-2 " />
-                            </div>
-                            <ul className="">
-                                <li><Link to="about" className="flex pl-8 py-2 w-auto border-b">Về chúng tôi</Link></li>
-                                <li><Link to="commit" className="flex pl-8 py-2 w-auto border-b">Cam kết</Link></li>
-                            </ul>
-                        </li>
-                        <li>
-                            <div className="flex justify-between items-center border-b">
-                                <Link to="tracking" className="inline-flex px-4 py-2 ">Tra cứu</Link>
-                                <FaChevronDown className=" h-3 w-3 mr-2 " />
-                            </div>
-                            <ul className=" ">
-                                <li><Link to="/" className="flex pl-8 py-2 w-auto border-b">Cước vận chuyển</Link></li>
-                                <li><Link to="/" className="flex pl-8 py-2 w-auto border-b">Bưu cục gần đây</Link></li>
-                                <li><Link to="/" className="flex pl-8 py-2 w-auto border-b">Vận đơn</Link></li>
-                                <li><Link to="/" className="flex pl-8 py-2 w-auto border-b">Bảng giá</Link></li>
-                                <li><Link to="/" className="flex pl-8 py-2 w-auto border-b">Hàng cấm gửi</Link></li>
-                            </ul>
-                        </li>
-                        <li>
-                            <div className="flex justify-between items-center border-b">
-                                <Link to="service" className="inline-flex px-4 py-2 ">Dịch vụ</Link>
-                                <FaChevronDown className=" h-3 w-3 mr-2 " />
-                            </div>
-                            <ul className="">
-                                <li>
-                                    <Link to="standard" className="flex flex-col pl-8 py-2 w-auto border-b ">
-                                        <span>Dịch vụ chuyển phát tiêu chuẩn</span>
-                                        <i className="text-[#F00] font-bold">J&T Epress</i>
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link to="fast-service" className="flex flex-col  pl-8 py-2 w-auto border-b ">
-                                        <span>Dịch vụ nhanh</span>
-                                        <i className="text-[#F00] font-bold">J&T Fast</i>
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link to="super-service" className="flex flex-col pl-8 py-2 w-auto border-b ">
-                                        <span>Dịch vụ siêu giao hàng</span>
-                                        <i className="text-[#F00] font-bold">J&T Fast</i>
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link to="fresh-service" className="flex flex-col pl-8 py-2 w-auto border-b ">
-                                        <span>Dịch vụ Tươi sống</span>
-                                        <i className="text-[#F00] font-bold">J&T Fresh</i>
-                                    </Link>
-                                </li>
-                            </ul>
-                        </li>
-                        <li>
-                            <div className="flex justify-between items-center border-b">
-                                <Link to="career" className="inline-flex px-4 py-2 ">Tuyển dụng</Link>
-                                <FaChevronDown className=" h-3 w-3 mr-2 " />
-                            </div>
-                            <ul className=" ">
-                                <li><Link to="career" className="flex pl-8 py-2 w-auto border-b">Cơ hội nghề nghiệp</Link></li>
-                                <li><Link to="cuoc-song-jnt" className="flex pl-8 py-2 w-auto border-b">Cuộc sống J&T Express</Link></li>
-                            </ul>
-                        </li>
-                        <li> <div className="flex justify-between items-center border-b">
-                            <Link to="consultant" className="inline-flex px-4 py-2 ">Tư vấn</Link>
-                            <FaChevronDown className=" h-3 w-3 mr-2 " />
-                        </div>
-                            <ul className=" ">
-                                <li><Link to="contact" className="flex pl-8 py-2 w-auto border-b">Liên hệ</Link></li>
-                                <li><Link to="consultant" className="flex pl-8 py-2 w-auto border-b">Đăng kí tư vấn</Link></li>
-                            </ul></li>
-                    </ul>
+                <div className="px-4 py-2 bg-yellow-500 hover:bg-opacity-70 rounded-md text-sm ">
+                    <a href="#" className="font-semibold">Đăng nhập</a>
                 </div>
             </div >
-        </div >
+
+            {/* Phần mobile menu */}
+            <div className={isOpen ? 'lg:hidden fixed left-0 top-[65px] right-0 bottom-0 bg-slate-400/40' : 'hidden lg:hidden fixed left-0 top-[65px] right-0 bottom-0 bg-slate-400/40'}
+                onClick={(removeMobileMenu)}
+            >
+                <Menu
+                    className="w-2/3 h-full"
+                    mode="inline"
+                    openKeys={openKeys}
+                    onOpenChange={onOpenChange}
+                    items={items}
+                />
+            </div >
+        </All >
     )
 }
 export default Header
