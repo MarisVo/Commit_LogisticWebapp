dac6a5f2b5718d9071bb5e83f08f3f7298b8a968
thong <leethong1997@gmail.com>
2022-06-27 14:57:15 +0700

    fix validate form

--- frontend/src/components/RecruitForm/RecruitForm.jsx
+++ frontend/src/components/RecruitForm/RecruitForm.jsx
@@ -1,15 +1,45 @@
 import { Form, Formik } from 'formik';
 import { useState } from 'react';
 import { FormGroup, Input, Label } from 'reactstrap';
+import isEmpty from 'validator/lib/isEmpty';
+import isEmail from 'validator/lib/isEmail';
 
 const RecruitForm = () => {
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [phone, setPhone] = useState('');
     const [birthday, setBirthday] = useState('');
+    const [validationMsg, setValidationMsg] = useState({});
+
+    const validateAll = () => {
+        const msg = {};
+        if (isEmpty(email)) {
+            msg.email = 'Vui lòng nhập email của bạn !';
+        } else if (!isEmail(email)) {
+            msg.email = 'Email không hợp lệ';
+        }
+
+        if (isEmpty(name)) {
+            msg.name = 'Vui lòng nhập họ và tên ';
+        }
+
+        if (isEmpty(phone)) {
+            msg.phone = 'Vui lòng nhập số điện thoại ';
+        }
+
+        if (isEmpty(birthday)) {
+            msg.birthday = 'Vui lòng nhập ngày sinh của bạn ';
+        }
+
+        setValidationMsg(msg);
+        if (Object.keys(msg).length > 0) return false;
+        return true;
+    };
 
     const handleSubmit = (e) => {
         e.preventDefault();
+        const isValid = validateAll();
+        if (!isValid) return;
         console.log({
             name,
             email,
@@ -26,27 +56,26 @@ const RecruitForm = () => {
         <Formik>
             {() => {
                 return (
-                    <div className="bg-[#fff] p-7 lg:p-14 w-[90%] lg:w-auto rounded-lg absolute top-[50%] left-[50%]  translate-y-[-50%]  translate-x-[-50%]">
+                    <div className="bg-[#fff] p-4 sm:p-7 lg:p-14 w-[90%] lg:w-auto rounded-lg absolute top-[50%] left-[50%]  translate-y-[-50%]  translate-x-[-50%]">
                         <h3 className="mb-2 text-3xl font-bold lg:mb-5">Đăng ký ứng tuyển</h3>
                         <Form onSubmit={handleSubmit} className="gap-4 lg:grid lg:grid-cols-2">
-                            <FormGroup className="flex flex-col">
+                            <FormGroup className="flex flex-col mb-0 lg:mb-3">
                                 <Label for="name">Họ và tên</Label>
                                 <Input
                                     className="py-2 border placeholder:pl-2 xl:w-[260px] lg:w-[200px]"
                                     id="name"
-                                    required
                                     type="text"
                                     name=""
                                     value={name}
                                     placeholder="Họ và tên"
                                     onChange={(e) => setName(e.target.value)}
                                 />
+                                <p className="mt-2 text-sm text-red-400 ">{validationMsg.name}</p>
                             </FormGroup>
 
-                            <FormGroup className="flex flex-col">
+                            <FormGroup className="flex flex-col mb-0 lg:mb-3">
                                 <Label for="email">Email *</Label>
                                 <Input
-                                    required
                                     className="py-2 border placeholder:pl-2 xl:w-[260px] lg:w-[200px]"
                                     type="email *"
                                     name="email"
@@ -55,12 +84,12 @@ const RecruitForm = () => {
                                     placeholder="Email"
                                     onChange={(e) => setEmail(e.target.value)}
                                 />
+                                <p className="mt-2 text-sm text-red-400 ">{validationMsg.email}</p>
                             </FormGroup>
 
-                            <FormGroup className="flex flex-col">
+                            <FormGroup className="flex flex-col mb-0 lg:mb-3">
                                 <Label for="phone">Số điện thoại *</Label>
                                 <Input
-                                    required
                                     className="py-2 border placeholder:pl-2 xl:w-[260px] lg:w-[200px]"
                                     type="number"
                                     name="phone"
@@ -69,12 +98,12 @@ const RecruitForm = () => {
                                     placeholder="Số điện thoại"
                                     onChange={(e) => setPhone(e.target.value)}
                                 />
+                                <p className="mt-2 text-sm text-red-400 ">{validationMsg.phone}</p>
                             </FormGroup>
 
-                            <FormGroup className="flex flex-col">
+                            <FormGroup className="flex flex-col mb-0 lg:mb-3">
                                 <Label for="birthday">Năm sinh *</Label>
                                 <Input
-                                    required
                                     className="py-2 border placeholder:pl-2 xl:w-[260px] lg:w-[200px]"
                                     type="date"
                                     name="birthday"
@@ -83,10 +112,11 @@ const RecruitForm = () => {
                                     placeholder="Năm sinh"
                                     onChange={(e) => setBirthday(e.target.value)}
                                 />
+                                <p className="mt-2 text-sm text-red-400 ">{validationMsg.birthday}</p>
                             </FormGroup>
 
                             <FormGroup className=" col-span-full">
-                                <button type="submit" className="w-[100%] bg-[#e5a663] py-2 mt-5 rounded-lg">
+                                <button type="submit" className="w-[100%] bg-[#e5a663] py-2 sm:mt-5 rounded-lg">
                                     Gửi đăng ký ứng tuyển
                                 </button>
                             </FormGroup>
--- frontend/src/pages/pageExport.js
+++ frontend/src/pages/pageExport.js
@@ -1,23 +1,22 @@
-export { default as Home } from "./home/Home";
-export { default as About } from "./about/About";
-export { default as Commit } from "./about/Commit";
+export { default as Home } from './home/Home';
+export { default as About } from './about/About';
+export { default as Commit } from './about/Commit';
 // ---------------------------------------------//
-export { default as Track } from "./track/Track";
-export { default as BangGia } from "./track/BangGia";
-export { default as VanDon } from "./track/VanDon";
-export { default as CuocVanChuyen } from "./track/CuocVanChuyen";
-export { default as BuuCuc } from "./track/BuuCuc";
-export { default as HangCamGui } from "./track/HangCamGui";
+export { default as Track } from './track/Track';
+export { default as BangGia } from './track/BangGia';
+export { default as VanDon } from './track/VanDon';
+export { default as CuocVanChuyen } from './track/CuocVanChuyen';
+export { default as BuuCuc } from './track/BuuCuc';
+export { default as HangCamGui } from './track/HangCamGui';
 // ---------------------------------------------//
-export { default as Adivce } from "./advice/Advice";
-export { default as Contact } from "./advice/contact/Contact.advice";
-export { default as SignUpAdvice } from "./advice/signUpForAdvise/SignUp.advice";
+export { default as Adivce } from './advice/Advice';
+export { default as Contact } from './advice/contact/Contact.advice';
+export { default as SignUpAdvice } from './advice/signUpForAdvise/SignUp.advice';
 //----------------------------------------------//
-export { default as CarerrOpportunity } from "./CareerOpportunities/CareerOpportunities.jsx";
-export { default as Life } from "./Life/Life.jsx";
-export { default as RecruimentDetail } from "./RecruitmentDetails/RecruitmentDetails.jsx";
+export { default as CareerOpportunities } from './CareerOpportunities/CareerOpportunities.jsx';
+export { default as Life } from './Life/Life.jsx';
+export { default as RecruitmentDetails } from './RecruitmentDetails/RecruitmentDetails.jsx';
 
+export { default as PageNotFound } from './PageNotFound';
 
-export { default as PageNotFound } from "./PageNotFound";
-
-export { default as DefaultLayout } from "../layouts/DefaultLayout";
+export { default as DefaultLayout } from '../layouts/DefaultLayout';
