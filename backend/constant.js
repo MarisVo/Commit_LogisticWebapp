8783195d4a4ed15240b36b4bd9afeefdbbebf540
mhung-afk <rairaikiri@gmail.com>
2022-07-14 10:01:25 +0700

    update db models

--- backend/constant.js
+++ backend/constant.js
@@ -113,6 +113,12 @@ export const ORDER_STATUS = {
     completed: 'completed'
 }
 
+export const BILL_STATUS = {
+    waiting: 'waiting',
+    processing: 'processing',
+    completed: 'completed'
+}
+
 export const NOTIFY_EVENT = {
     connection: 'connection',
     addSession: 'add-session',
