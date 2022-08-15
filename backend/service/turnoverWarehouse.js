0ad118e9da181adae364b016a78f0896022814e0
Thiện Thành Nguyễn <109054326+tian-nguyen@users.noreply.github.com>
2022-08-15 18:24:43 +0700

    quan ly doan thu warehouse

---
+++ backend/service/turnoverWarehouse.js
@@ -0,0 +1,17 @@
+import Warehouse from "../model/Warehouse.js";
+
+
+export const calculateWarehouseTurnover = async (warehouseId) => {
+   try { 
+    const warehouse = await Warehouse.findById(warehouseId);
+    if (!warehouse) {return sendError(res,"No warehouse found");}
+    let sum = 0;
+    for (let i = 0; i < warehouse.inventory_product_shipments.length; i++) {
+        sum += (warehouse.inventory_product_shipments[i].turnover)
+    }
+    return sum
+    }
+    catch (err) {
+        console.log(err);
+    }
+}
