7ddeab4be899745750842adddda2e257638f0d94
mhung-afk <rairaikiri@gmail.com>
2022-08-21 20:13:49 +0700

    merge fe

--- backend/index.js
+++ backend/index.js
@@ -87,7 +87,7 @@ app.use(bodyParser.json())
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
     .use('/api/public', publicRoute)
-    .use('/api/admin',/* verifyToken, verifyAdmin ,*/ adminRoute)
+    .use('/api/admin', verifyToken, verifyAdmin, adminRoute)
     .use('/api/auth', authRoute)
     .use('/api/tracking', trackingRoute)
     .use('/api/order', orderRoute)
